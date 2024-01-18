8a5047b9ac5b1011ff233497dd8e790b213ec283
zacharba <70348037+zacharba@users.noreply.github.com>
2024-01-18 16:37:16 -0500

    A few changes made that weren't committed

--- public/index.js
+++ public/index.js
@@ -6,13 +6,13 @@ const rightArrow = document.querySelector('.right');
 var sectionIndex = 0;
 
 leftArrow.addEventListener('click', function() {
-	sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 1;
-	slider.style.transform = 'translate(' + (sectionIndex) * -50 + '%)';
-	sliderDescription.style.transform = 'translate(' + (sectionIndex) * -50 + '%)';
+	sectionIndex = (sectionIndex > 0) ? sectionIndex - 1 : 2;
+	slider.style.transform = 'translate(' + (sectionIndex) * -33.33 + '%)';
+	sliderDescription.style.transform = 'translate(' + (sectionIndex) * -33.33 + '%)';
 });
 
 rightArrow.addEventListener('click', function() {
-	sectionIndex = (sectionIndex < 1) ? sectionIndex + 1 : 0;
-	slider.style.transform = 'translate(' + (sectionIndex) * -50 + '%)';
-	sliderDescription.style.transform = 'translate(' + (sectionIndex) * -50 + '%)';
+	sectionIndex = (sectionIndex < 2) ? sectionIndex + 1 : 0;
+	slider.style.transform = 'translate(' + (sectionIndex) * -33.33 + '%)';
+	sliderDescription.style.transform = 'translate(' + (sectionIndex) * -33.33 + '%)';
 });
